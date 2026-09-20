2998bfdca69cf4ff6e171abda78ae5ca8ffd0d5f
Siddharth <siddharth.obr@gmail.com>
2026-09-21 00:48:40 +0530

    g

---
+++ Bussiness_Name_Generator.js
@@ -0,0 +1,34 @@
+// Create  a bussiness name generator by combinning list of objective and shop name and another wordBreak: 
+
+Adjectives:
+    // Crazy
+    //Amazing
+    //Fire
+
+    // Shop Name
+    // Engine
+    // Foods
+    // Garments
+
+    // Another Word:
+    // bors 
+    // limited
+    // hub
+
+    const adjectives = {
+        C: "Crazy",
+        A: "Amazing",
+        F: "Fire"
+    }
+
+const shopNames = {
+    E: "Engine",
+    F: "Foods",
+    G: "Garments"
+}
+
+const anotherWords = {
+    B: "Bors",
+    L: "Limited",
+    H: "Hub"
+}
