22d9eefaa24dfcf5e214e0cda5927f2ec56996ec
Siddharth Yadav <siddharth.obr@gmail.com>
2026-09-20 20:36:10 +0530

    Create Practice_Set_3.js

---
+++ Practice_Set_3.js
@@ -0,0 +1,50 @@
+// //  Question 1st
+
+
+// let object = {
+//     harry: 98,
+//     rohan: 70,
+//     aakash: 7
+// }
+
+// console.log(object);
+
+
+
+// //  Question 2nd
+
+// for (const key in object) {
+
+
+//     const element = object[key, object];
+
+
+// }
+
+
+// //  question 3rd
+
+// let numberCorrect = 4;
+// let numbers;
+
+// while (numbers !== numberCorrect) {
+//     numbers = Number(prompt("Enter the correct number"));
+
+//     if (numbers == numberCorrect) {
+//         console.log("You have entered the correct number");
+//     } else {
+//         console.log("Try again");
+//     }
+// }
+
+
+
+
+
+// question 4th
+
+function mean(a, b, c, d, e) {
+    return (a + b + c + d + e) / 5;
+}
+let result = mean(10, 20, 30, 40, 50);
+console.log(result);
