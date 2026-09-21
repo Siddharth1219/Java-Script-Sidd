f3dafedda2c8883c2d28b0c405f86b3721093ea4
Siddharth <siddharth.obr@gmail.com>
2026-09-21 23:47:36 +0530

    Add DOM element selection demo files
    Introduce a new HTML page and linked JavaScript to demonstrate basic DOM selection methods. The page adds multiple `.box` elements, and the script shows usage of `getElementsByClassName` and `getElementById` by logging selected elements and applying background colors to specific boxes.

---
+++ Selecting.js
@@ -0,0 +1,25 @@
+console.log("Siddharht Yadav");
+
+
+let boxes = document.getElementsByClassName("box");
+console.log(boxes);
+
+boxes[5].style.backgroundColor = "green";
+
+
+
+//  getElementById is used to select a single element by its ID
+
+document.getElementById("red").style.backgroundColor = "red";
+
+
+
+
+//  querySelector is used to select the first element that matches a CSS selector
+
+// document.querySelector(".box").forEach(e => {
+//     e.style.backgroundColor = "blue"
+// })
+
+
+//
