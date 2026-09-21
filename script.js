a99ed06aa1a140768434fc732a849e68f14f2cd5
Siddharth <siddharth.obr@gmail.com>
2026-09-21 21:24:18 +0530

    Create basic DOM box demo page
    Updates Index.html from a variables example to a DOM-focused layout with six styled box elements and loads a new script.js file. Adds script.js with an initial console log and commented DOM traversal references for child, sibling, and node access while learning/documenting DOM navigation.

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+
+
+// document.body.firstElementChild
+
+// document.body.firstElementChild.childNodes
+
+// NodeList(13) [text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text]
+
+// document.body.firstElementChild.children
+
+// HTMLCollection(6) [div.box, div.box, div.box, div.box, div.box, div.box]
+
+// document.body.firstElementChild.children[0]
+
+// document.body.firstElementChild.children[3].nextElementSibling
+
+// document.body.firstElementChild.children[3].previousElementSibling
+
+// document.body.firstElementChild.children[3].previousSibling
