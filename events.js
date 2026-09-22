b429c2672057785aeaf549670ba3b0245dddc00b
Siddharth <siddharth.obr@gmail.com>
2026-09-22 20:00:02 +0530

    Add event bubbling demo and event handlers
    Introduces a new `eventBubbling.html` page to demonstrate bubbling and propagation control with nested elements. Updates `events.html` to load `events.js` at the end of the body so DOM elements are available before binding listeners, and expands `events.js` with practical mouse/keyboard event examples (`dblclick`, `contextmenu`, and `keydown`) plus reference links. Also adds basic VS Code workspace settings for debug terminal launch and Live Server port.

--- events.js
+++ events.js
@@ -1,3 +1,38 @@
+//  mouse events list link -->>  https://developer.mozilla.org/en-US/docs/Web/Events#mouse_events --> 
+
+
 let button = document.getElementById("btn");
 
-button.addEventListener("click", () => {)
+// ---------Click--------------
+// button.addEventListener("click", () => {
+//     alert("I was clicked. Yay!!");
+//     document.querySelector(".box").innerHTML = "<b>Yay I was clicked.</b> Enjoy your Click";
+// })
+
+
+
+// ---------Double Click--------------
+button.addEventListener("dblclick", () => {
+    alert("Doubled Clicked. Yay!!");
+    document.querySelector(".box").innerHTML = "<b>Yay I was doubled clicked.</b> Enjoy your Click";
+})
+
+
+// ---------Context Menu (Right Click)--------------
+button.addEventListener("contextmenu", () => {
+    alert("Hack us by right: Clicking. Yay!!");
+    document.querySelector(".box").innerHTML = "<b>Yay I was right clicked.</b> Enjoy your hacked Click";
+})
+
+
+
+
+
+
+// -----KEYBOARD EVENTS LIST LINK -->>  https://developer.mozilla.org/en-US/docs/Web/Events#keyboard_events --> 
+
+
+// ---------Keydown--------------
+button.addEventListener("keydown", (e) => {
+    console.log(e);
+})
